9595012526b918bde6802e2f8f9a981e176dabd0
OpasniDed <122799643+OpasniDed@users.noreply.github.com>
2023-08-25 20:37:07 +0300

    z

---
+++ markups.py
@@ -0,0 +1,38 @@
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton#, ReplyKeyboardRemove
+
+btnMain = KeyboardButton('⬅ Главное меню')
+
+
+# Главное меню
+btnBuy = KeyboardButton('💰 Купить')
+btnInfo = KeyboardButton('ℹ Подробнее')
+btnInfoGame = KeyboardButton('ℹ Об Игре')
+btndop = KeyboardButton('↪ Дополнительно')
+mainMenu = ReplyKeyboardMarkup(resize_keyboard=True).add(btnBuy, btnInfo, btnInfoGame, btndop)
+
+
+# Меню + факты
+btnmenu = KeyboardButton('⬅ Меню')
+b1 = KeyboardButton('Мираж')
+b2 = KeyboardButton('Онрё')
+b3 = KeyboardButton('Банши')
+otherMenu = ReplyKeyboardMarkup(resize_keyboard=True).add(b3, b1, b2, btnmenu)
+
+
+
+
+# b1 = KeyboardButton('Купить')
+# b2 = KeyboardButton('Подробнее')
+# b3 = KeyboardButton('Об Игре')
+# b4 = KeyboardButton('Дополнительно')
+
+
+# Тестовые кнопки
+# bt1 = KeyboardButton('Open steam')
+
+
+# markup = ReplyKeyboardMarkup(resize_keyboard=True)
+
+# markup.add(b1)
+# markup.row(b2, b3)
+
